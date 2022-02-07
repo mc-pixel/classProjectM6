c2e9ffae9f76aa25f6fd13bd68e128810b199cd8
miguel celma <mrcelma3@gmail.com>
2022-02-07 15:04:48 +0100

    Initial commit

---
+++ src/main/java/B.java
@@ -0,0 +1,8 @@
+
+public class B extends A {
+    public static void main(String[] args)
+    {
+        String b;
+        this.b=a;
+    }
+}
---
+++ src/main/java/C.java
@@ -0,0 +1,8 @@
+import class.B;
+public class C extends B{
+    public static void main(String[] args)
+    {
+        String c;
+        this.c = b;
+    }
+}
